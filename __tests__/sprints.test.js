d60caa27c7ace2373a2213e2372f87ab59baf5a0
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-26 17:02:16 -0400

    passing post test

---
+++ __tests__/sprints.test.js
@@ -0,0 +1,63 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Sprint from '../lib/models/Sprint.js'
+import AuthService from '../lib/services/AuthService.js'
+
+describe('sprints routes', () => {
+
+  const agent = request.agent(app);
+  let user;
+
+  beforeEach(async() => {
+    await setup(pool);
+
+    user = (await agent
+      .post('/api/v1/auth/signup')
+      .send({
+      name: 'clem',
+      email: 'clem@clem.clem',
+      password: 'password',
+      cohort: 'march 2021'
+    })).body;
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'clem@clem.clem',
+        password: 'password'
+      });
+
+    return user;
+  });
+
+  const sprint1 = {
+    name: 'mid project',
+    cohort: 'march 2021',
+  };
+
+  const sprint2 = {
+    name: 'final project',
+    cohort: 'july 2021',
+  };
+
+  const sprint3 = {
+    name: 'final project',
+    cohort: 'march 2021',
+  };
+
+  it.only('creates a sprint', async () => {
+    const res = await agent
+      .post('/api/v1/sprints')
+      .send(sprint1)
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'mid project',
+      cohort: 'march 2021',
+      result: null,
+      createdAt: expect.any(String)
+    })
+  })
+})
