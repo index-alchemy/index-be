55fb930f52a76618a421a3a7896ead8aba2ae1fb
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-26 13:34:36 -0700

    login user via post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,10 +16,33 @@ describe('auth routes', () => {
     return setup(pool);
   });
 
-  test.only('Post a user to auth/signup', async () => {
+  test('Post a user to auth/signup', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
       .send(user);
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: user.name,
+      email: user.email,
+      cohort: user.cohort,
+      passwordHash: expect.any(String),
+      createdAt: expect.any(String),
+    });
+  });
+
+  test('logs a user in via Post', async () => {
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: user.email,
+        password: user.password
+      });
 
     expect(res.body).toEqual({
       id: '1',
--- __tests__/pitches.test.js
+++ __tests__/pitches.test.js
@@ -1,59 +1,59 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
-import Pitch from '../lib/models/pitch.js';
-import AuthService from '../lib/services/AuthService.js';
+// import pool from '../lib/utils/pool.js';
+// import setup from '../data/setup.js';
+// import request from 'supertest';
+// import app from '../lib/app.js';
+// import Pitch from '../lib/models/pitch.js';
+// import AuthService from '../lib/services/AuthService.js';
 
 
-describe('pitch routes', () => {
+// describe('pitch routes', () => {
 
-  const agent = request.agent(app);
-  let user;
+//   const agent = request.agent(app);
+//   let user;
 
-  beforeEach(async () => {
-    await setup(pool);
+//   beforeEach(async () => {
+//     await setup(pool);
 
-    user = await AuthService.create({
-      name: 'test',
-      password: 'test',
-      email: 'email@test.com',
-      cohort: 'Mar 2021'
-    });
+//     user = await AuthService.create({
+//       name: 'test',
+//       password: 'test',
+//       email: 'email@test.com',
+//       cohort: 'Mar 2021'
+//     });
 
-    await agent
-      .post('/api/v1/auth/login')
-      .send({
-        email: 'email@test.com',
-        password: 'test'
-      });
+//     await agent
+//       .post('/api/v1/auth/login')
+//       .send({
+//         email: 'email@test.com',
+//         password: 'test'
+//       });
 
-    return user;
-  });
+//     return user;
+//   });
 
-  const sprint = {
-    id: '1',
-    name: 'sprint 1',
-    cohort: 'Mar 2021',
-    results: ['a', 'b', 'c'],
-    createdAt: expect.any(String)
-  };
+//   const sprint = {
+//     id: '1',
+//     name: 'sprint 1',
+//     cohort: 'Mar 2021',
+//     results: ['a', 'b', 'c'],
+//     createdAt: expect.any(String)
+//   };
 
-  // create a test that will create a pitch
-  it('creates a pitch via POST', async () => {
-    const res = await Pitch.create({
-      userId: user.id,
-      sprintId: sprint.id,
-      pitch: 'this is a pitch',
-      description: 'this is a description of a pitch'
-    });
-    console.log(res);
-    expect(res.body).toEqual({
-      id: '1',
-      userId: user.id,
-      sprintId: sprint.id,
-      pitch: 'this is a pitch',
-      description: 'this is a description of a pitch'
-    });
-  });
-});
+//   // create a test that will create a pitch
+//   it('creates a pitch via POST', async () => {
+//     const res = await Pitch.create({
+//       userId: user.id,
+//       sprintId: sprint.id,
+//       pitch: 'this is a pitch',
+//       description: 'this is a description of a pitch'
+//     });
+//     console.log(res);
+//     expect(res.body).toEqual({
+//       id: '1',
+//       userId: user.id,
+//       sprintId: sprint.id,
+//       pitch: 'this is a pitch',
+//       description: 'this is a description of a pitch'
+//     });
+//   });
+// });
