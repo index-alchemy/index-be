342da046ff1e1641214120f0e053cca40293fccc
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-29 13:17:35 -0700

    create load-seed-data file

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,1 +1,25 @@
 import sprints from './sprints';
+import client from '../lib/utils/client.js';
+
+run();
+
+async function run() {
+
+  try {
+    await Promise.all(
+      sprints.map(sprint => {
+        return client.query(`
+        INSERT INTO sprints (name, cohort)
+        VALUES ($1, $2)
+        RETURNING *;
+        `, [sprint.name, sprint.cohort]);
+      })
+    );
+  }
+  catch (err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+}
