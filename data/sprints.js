76f52ebbbaa78939ce10aa57045fb8ce30d7e356
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-29 11:47:31 -0700

    create sprint data

---
+++ data/sprints.js
@@ -0,0 +1,218 @@
+sprints = [
+  {
+    "name": "Foundations I",
+    "cohort": "Spring 2019",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Spring 2019",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Spring 2019",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Spring 2019",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "Summer 2019",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Summer 2019",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Summer 2019",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Summer 2019",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "Fall 2019",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Fall 2019",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Fall 2019",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Fall 2019",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "Winter 2020",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Winter 2020",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Winter 2020",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Winter 2020",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "Spring 2020",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Spring 2020",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Spring 2020",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Spring 2020",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "Summer 2020",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Summer 2020",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Summer 2020",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Summer 2020",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "Fall 2020",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "Fall 2020",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "Fall 2020",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "Fall 2020",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "January 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "January 2021",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "January 2021",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "January 2021",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "March 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "March 2021",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "March 2021",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "March 2021",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "April 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "April 2021",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "April 2021",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "April 2021",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "June 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "June 2021",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "June 2021",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "June 2021",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "August 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "August 2021",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "August 2021",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "August 2021",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "September 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "September 2021",
+  },
+  {
+    "name": "Career Track Mid",
+    "cohort": "September 2021",
+  },
+  {
+    "name": "Career Track Final",
+    "cohort": "September 2021",
+  },
+  {
+    "name": "Foundations I",
+    "cohort": "November 2021",
+  },
+  {
+    "name": "Foundations II",
+    "cohort": "November 2021",
+  }
+];
