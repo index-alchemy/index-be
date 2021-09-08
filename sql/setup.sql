8b39eaf6979cd6ae97dc4dc1b811fb51dd9c78d9
culi <culitif@tuta.io>
2021-09-07 22:16:52 -0700

    add algolia pitch service

--- sql/setup.sql
+++ sql/setup.sql
@@ -28,6 +28,7 @@ CREATE TABLE pitches (
   sprint_id BIGINT REFERENCES sprints(id) ON DELETE CASCADE,
   pitch TEXT NOT NULL,
   description TEXT NOT NULL,
+  completed TEXT DEFAULT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
